3c520eb56c040dfa4c4fd2bb4cf736b0b86bce2e
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-05 16:59:26 +0200

    [dw/gui/configuration] Creating webpack configuration working in development mode.

--- gui/config/webpack.config.js
+++ gui/config/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path")
-require("dotenv").config({path: path.resolve(__dirname, "../.env")})
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
+
+const HtmlWebpackPlugin = require("webpack-html-plugin")
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
 
 const ExitWithMessage = (msg, tip) => {
   console.error(`Error: ${msg}\n${tip}`)
@@ -11,4 +14,40 @@ if (process.env.MODE == undefined || process.env.PORT == undefined) {
     "Cannot load environment variables.",
     `Try to check ${__filename} file.`
   )
+}
+
+module.exports = {
+  mode: process.env.MODE,
+
+  entry: path.resolve(__dirname, "../src"),
+
+  resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'] },
+
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist"), 
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(js|ts)x?$/i,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin( {template: path.resolve(__dirname, "./src/index.html")} )
+  ],
+
+  devServer: {
+    port: process.env.PORT
+  }
 }
