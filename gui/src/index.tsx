ce71950e830ed7bb6190fe982645bbb0a5166e4b
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-06 22:38:45 +0200

    [dw/gui/configuration] Moving from webpack to vite.

--- gui/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom/client"
-
-const Index = (props) => {
-  return <h1>Hello, World!</h1>
-} 
-
-const root = ReactDOM.createRoot(document.querySelector("#root"))
-root.render(<Index />)
