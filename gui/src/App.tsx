0641706b16dc1deb692988209c78187a2f49eef0
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-08 16:41:37 +0200

    [dw/gui/auth] Installing tailwindcss packages and configuring the tailwindcss.

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import AuthPage from './pages/authPage'
+import './index.css'
 
 const App = () => {
     return (
