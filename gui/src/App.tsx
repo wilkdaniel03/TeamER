8d2cfe955899fe728150dea06919bb974a743835
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-08 16:33:53 +0200

    [dw/gui/auth] Adding react router boiler plate and creating auth route and redirection to auth page.

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,5 +1,16 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import AuthPage from './pages/authPage'
+
 const App = () => {
-    return <h1>Hello TeamER!</h1>
+    return (
+        <main>
+            <Routes>
+                <Route path="/" element={<Navigate replace to="/auth" />} />
+                <Route path="auth" element={<AuthPage />} />
+            </Routes>
+        </main>
+    )
 }
 
 export default App
