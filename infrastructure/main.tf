47496357eda890da3e08af0a9ecd55dcc128d34e
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-12 14:05:41 +0200

    [dw/local-infrastructure] Adding linode provider to configuration.

---
+++ infrastructure/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "~> 1.27.0"
+    }
+  }
+  required_version = "~> 1.3.0"
+}
+
+provider "linode" {
+
+}
