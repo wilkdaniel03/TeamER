93e04cbdb0bcfc369336b1f01f2818178e132774
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-13 19:28:36 +0200

    [dw/gui/auth] Adding confirm button to auth page.

---
+++ gui/src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+interface ButtonProps {
+  value: string
+  link: string
+}
+
+const classes: string =
+  'bg-blue text-white py-3 px-6 font-medium text-xl rounded-lg w-full'
+
+export const Button: React.FC<ButtonProps> = ({ value, link }) => {
+  return (
+    <Link to={link}>
+      <button className={classes}>{value}</button>
+    </Link>
+  )
+}
