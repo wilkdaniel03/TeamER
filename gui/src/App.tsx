38e60bfb829b011048f63978d258a685c2b80824
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-13 21:41:22 +0200

    [dw/gui/auth] Creating auth signin route.

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import AuthPage from './pages/authPage'
-import { AuthPageRoute } from './pages
+import { AuthPageRoute, AuthPageSignInRoute } from './pages'
 
 const App = () => {
   return (
@@ -10,6 +10,7 @@ const App = () => {
         <Route path="/" element={<Navigate replace to="/auth" />} />
         <Route path="auth" element={<AuthPage />}>
           <Route index element={<AuthPageRoute />} />
+          <Route path="signin" element={<AuthPageSignInRoute />} />
         </Route>
       </Routes>
     </main>
