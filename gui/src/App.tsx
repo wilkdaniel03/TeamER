46ccdf8ffb39cc3f124d41d9627fb8a1e2684f97
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-13 14:06:58 +0200

    [dw/gui/auth] Moving tailwindcss's styles declaration's to main.tsx .

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -3,14 +3,14 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import AuthPage from './pages/authPage'
 
 const App = () => {
-    return (
-        <main>
-            <Routes>
-                <Route path="/" element={<Navigate replace to="/auth" />} />
-                <Route path="auth" element={<AuthPage />} />
-            </Routes>
-        </main>
-    )
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/auth" />} />
+        <Route path="auth" element={<AuthPage />} />
+      </Routes>
+    </main>
+  )
 }
 
 export default App
