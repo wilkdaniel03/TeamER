7867eb6b981fa45fd18edf87849215386eb4aff3
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-13 19:01:28 +0200

    [dw/gui/auth] Adding few values to tailwind config.

--- gui/tailwind.config.js
+++ gui/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       white: '#fff',
       blue: '#1363df',
       black: '#000',
+      gray: '#7f8487',
       extend: {},
+    },
+    borderWidth: {
+      1: '1px',
     },
     variants: {
       extend: {},
