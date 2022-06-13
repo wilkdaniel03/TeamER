acd8c7d289e6ce9a06385b05260034fb51796b97
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-13 21:36:36 +0200

    [dw/gui/auth] Creating auth index route.

--- gui/src/pages/authPage.tsx
+++ gui/src/pages/authPage.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import facebookIcon from '../img/facebook.png'
-import googleIcon from '../img/google.png'
 import AuthPageHeading from './AuthPageHeading'
 import { Container, Button } from '../components'
+import { Outlet } from 'react-router-dom'
 
 const AuthPage = () => {
   return (
@@ -11,22 +10,7 @@ const AuthPage = () => {
         <header className="flex flex-col items-center">
           <AuthPageHeading />
         </header>
-        <footer className="flex flex-col items-center">
-          <Button value={'Create an Account'} />
-          <div className="w-full h-0.5 bg-gray mt-8 opacity-60">
-            <span className="block relative text-center bg-white w-2/5 left-1/2 -translate-x-1/2 -translate-y-1/2">
-              or Sign up with
-            </span>
-          </div>
-          <div className="flex justify-evenly w-2/3">
-            <span className="w-16 h-16 flex justify-center items-center mt-8 rounded-full shadow-md">
-              <img src={facebookIcon} width="48px" />
-            </span>
-            <span className="w-16 h-16 flex justify-center items-center mt-8 rounded-full shadow-md">
-              <img src={googleIcon} width="40px" />
-            </span>
-          </div>
-        </footer>
+        <Outlet />
       </>
     </Container>
   )
