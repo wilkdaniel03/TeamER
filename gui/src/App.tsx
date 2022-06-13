9542e92d19f16454a28d271d2d7d368b75f84959
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-13 21:36:36 +0200

    [dw/gui/auth] Creating auth index route.

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import AuthPage from './pages/authPage'
+import { AuthPageRoute } from './pages
 
 const App = () => {
   return (
     <main>
       <Routes>
         <Route path="/" element={<Navigate replace to="/auth" />} />
-        <Route path="auth" element={<AuthPage />} />
+        <Route path="auth" element={<AuthPage />}>
+          <Route index element={<AuthPageRoute />} />
+        </Route>
       </Routes>
     </main>
   )
