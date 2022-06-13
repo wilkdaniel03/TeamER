8a73ea28843e615c5721127b4393eba2169320b1
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-13 19:33:59 +0200

    [dw/gui/auth] Adding social media icons.

--- gui/src/pages/authPage.tsx
+++ gui/src/pages/authPage.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import facebookIcon from '../img/facebook.png'
+import googleIcon from '../img/google.png'
 import AuthPageHeading from './AuthPageHeading'
 import { Container, Button } from '../components'
 
@@ -15,6 +17,14 @@ const AuthPage = () => {
           <div className="w-full h-0.5 bg-gray mt-8 opacity-60">
             <span className="block relative text-center bg-white w-2/5 left-1/2 -translate-x-1/2 -translate-y-1/2">
               or Sign up with
+            </span>
+          </div>
+          <div className="flex justify-evenly w-2/3">
+            <span className="w-16 h-16 flex justify-center items-center mt-8 rounded-full shadow-md">
+              <img src={facebookIcon} width="48px" />
+            </span>
+            <span className="w-16 h-16 flex justify-center items-center mt-8 rounded-full shadow-md">
+              <img src={googleIcon} width="40px" />
             </span>
           </div>
         </footer>
